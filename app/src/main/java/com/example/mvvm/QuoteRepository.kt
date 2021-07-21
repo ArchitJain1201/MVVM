af5611b9a8a7dd1d80f0e65773ff8310fbbd107f
ArchitJain1201 <80871543+ArchitJain1201@users.noreply.github.com>
2021-07-21 12:41:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mvvm/QuoteRepository.kt
@@ -0,0 +1,12 @@
+package com.example.mvvm
+
+import androidx.lifecycle.LiveData
+
+class QuoteRepository(private val quoteDao: QuoteDao) {
+    fun getQuotes(): LiveData<List<Quote>>{
+        return quoteDao.getQuotes()
+    }
+    suspend fun insertQuote(quote: Quote){
+        quoteDao.insertQuote(quote)
+    }
+}
